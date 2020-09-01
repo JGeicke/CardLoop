473d8972e42469f4326c77c0aab7d1b7970c74d1
Richard Leichner <richard.leichner@mni.thm.de>
2020-09-01 21:54:58 +0200

    lessons with carousel

--- mc-trainer/src/app/loggedIn/logged-in/logged-in.page.ts
+++ mc-trainer/src/app/loggedIn/logged-in/logged-in.page.ts
@@ -1,19 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-logged-in',
-  templateUrl: './logged-in.page.html',
-  styleUrls: ['./logged-in.page.scss'],
+    selector: 'app-logged-in',
+    templateUrl: './logged-in.page.html',
+    styleUrls: ['./logged-in.page.scss'],
 })
 export class LoggedInPage implements OnInit {
 
-  constructor() { }
+    recommendations: any[] = [];
+    recommendation = {name: 'Compilerbau', cards: 42, tags: ['Programming', 'Computer Science', 'Something more']};
+    sliderConfig = {
+        spaceBetween: 10,
+        slidesPerView: 1.4
+    };
 
-  ngOnInit() {
-  }
+    constructor() {
+        this.recommendations.push(this.recommendation);
+        this.recommendations.push(this.recommendation);
+        this.recommendations.push(this.recommendation);
 
-  playLesson(){
-    console.log('playlesson called');
-  }
+    }
+
+    ngOnInit() {
+    }
+
+    playLesson() {
+        console.log('playlesson called');
+    }
+
+    lessonDetails() {
+        console.log('lessondetails called');
+    }
 
 }
