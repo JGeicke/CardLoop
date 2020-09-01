5bd30f67e006593655880b91db8084c2466c4875
Leona Kirchem <leona.kirchem@mni.thm.de>
2020-09-01 11:47:11 +0200

    achievement icons positioned

--- mc-trainer/src/app/achievement/achievement/achievement.module.ts
+++ mc-trainer/src/app/achievement/achievement/achievement.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { AchievementPageRoutingModule } from './achievement-routing.module';
+import {AchievementPageRoutingModule} from './achievement-routing.module';
 
-import { AchievementPage } from './achievement.page';
+import {AchievementPage} from './achievement.page';
+import {Achievement} from '../../../services/achievement.model';
+import {RoundProgressModule} from 'angular-svg-round-progressbar';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AchievementPageRoutingModule
+    AchievementPageRoutingModule,
+    RoundProgressModule
   ],
   declarations: [AchievementPage]
 })
-export class AchievementPageModule {}
+export class AchievementPageModule {
+}
--- mc-trainer/src/app/achievement/achievement/achievement.page.ts
+++ mc-trainer/src/app/achievement/achievement/achievement.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Achievement} from '../../../services/achievement.model';
+import {RoundProgressModule} from 'angular-svg-round-progressbar';
 
 @Component({
   selector: 'app-achievement',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AchievementPage implements OnInit {
 
-  constructor() { }
+  achievements: Achievement[] = [];
+  prog: Achievement = new Achievement('1', 'CardStreak',
+      'Achieve a streak of 6 right answered cards in a row!', 3, 6, './src/assets/mountain_with_flag');
+
+  constructor() {
+    console.log('hi');
+    this.achievements.push(new Achievement('1', 'CardStreak',
+        'Achieve a streak of 6 right answered cards in a row!', 3, 6, './src/assets/mountain_with_flag'));
+  }
 
   ngOnInit() {
   }
