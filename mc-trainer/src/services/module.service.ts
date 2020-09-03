c2f8b0c1fefab80c79e4024d2093e31ee74000f1
Richard Leichner <richard.leichner@mni.thm.de>
2020-09-03 15:25:04 +0200

    lessons mit modal für Löschungsbestätigung

--- mc-trainer/src/services/module.service.ts
+++ mc-trainer/src/services/module.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {AlertController, ModalController} from '@ionic/angular';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class ModuleService {
   private lesson = {name: 'Compilerbau', cards: 42, tags: ['Programming', 'Computer Science', 'Something more']};
 
 
-  constructor() {
+  constructor(private alertController: AlertController) {
     for (let i = 0; i < 5; i++) {
       this.lessons.push(this.lesson);
     }
@@ -18,6 +20,35 @@ export class ModuleService {
 
   lessonDetails(currLesson) {
     console.log(currLesson);
+  }
+
+  async delDialog() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Please confirm!',
+      message: '<p>This item will and all associated ' +
+          'data will be deleted. This action cannot be undone. </p>',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            this.deleteLesson(this.currLesson);
+            this.alertController.dismiss({
+              dismissed: true
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
   deleteLesson(currLesson) {
