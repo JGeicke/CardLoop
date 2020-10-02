61586e464b69ee5835ed6c5cf49062c71dbb7fb0
Richard Leichner <richard.leichner@mni.thm.de>
2020-10-02 10:28:23 +0200

    achievement statistics

--- mc-trainer/src/app/achievement/achievement/achievement.page.ts
+++ mc-trainer/src/app/achievement/achievement/achievement.page.ts
@@ -14,14 +14,50 @@ import {AchievementService} from '../../../services/achievement.service';
 // Achievement Page Front End Logic
 export class AchievementPage implements OnInit {
 
+  // statistics variables
+  picked: string;
   detailAchievement: boolean;
   currAchievement: Achievement;
+  avgsessiontime: number;
+  correctQuestCount: number;
+  wrongQuestCount: number;
+  lessonCount: number;
+  questCount: number;
+  regDate: string;
+
+  data = [];
+  view: any[] = [300, 300];
+
+  // piechart variables
+
+  gradient = false;
+  showLegend = false;
+  showLabels = false;
+  isDoughnut = false;
+  legendPosition = 'below';
+
+  colorScheme = {
+    domain: ['#66bd92', '#cf5d5d']
+  };
 
   constructor(private statisticService: StatisticService, private authService: AuthService,
               private moduleService: ModuleService, private achievementService: AchievementService) {
   }
 
   ionViewWillEnter(){
+    this.data = [
+      {
+        name: 'Correct',
+        value: this.correctQuestCount
+      },
+      {
+        name: 'Wrong',
+        value: this.wrongQuestCount
+      },
+    ];
+    console.log(typeof this.correctQuestCount);
+    console.log(typeof this.questCount);
+    this.picked = 'statistics';
     this.detailAchievement = false;
     this.achievementService.generateAchievements(this.moduleService.userModules.length);
     this.achievementService.sortAchievements();
@@ -38,6 +74,12 @@ export class AchievementPage implements OnInit {
     }
 
   ngOnInit() {
+    this.avgsessiontime = 10.4736;
+    this.correctQuestCount = 7;
+    this.questCount = 12;
+    this.lessonCount = 10;
+    this.wrongQuestCount = this.questCount - this.correctQuestCount ;
+    this.regDate = '29.07.2020';
   }
 
 }
