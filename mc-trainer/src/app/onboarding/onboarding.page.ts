8d8123fb43fc9674d2d7aaaa1d9b105f8a39b4a6
Leona Kirchem <leona.kirchem@mni.thm.de>
2020-10-05 16:39:25 +0200

    onboarding updated

--- mc-trainer/src/app/onboarding/onboarding.page.ts
+++ mc-trainer/src/app/onboarding/onboarding.page.ts
@@ -9,7 +9,7 @@ import {Router} from '@angular/router';
 export class OnboardingPage implements OnInit {
 
   constructor(private router: Router) {
-    if (!!localStorage.getItem('newUser')) {
+    if (JSON.stringify(localStorage.getItem('newUser')) == null) {
       this.router.navigate(['login']);
     }
   }
