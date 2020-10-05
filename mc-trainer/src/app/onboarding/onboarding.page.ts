4f4daae164bb7e38b4a814b0f85d89fc06eb3989
Leona Kirchem <leona.kirchem@mni.thm.de>
2020-10-05 10:34:57 +0200

    about-page done

--- mc-trainer/src/app/onboarding/onboarding.page.ts
+++ mc-trainer/src/app/onboarding/onboarding.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-onboarding',
@@ -7,8 +8,14 @@ import {Component, OnInit} from '@angular/core';
 })
 export class OnboardingPage implements OnInit {
 
-  constructor() {
+  constructor(private router: Router) {
+    if (!!localStorage.getItem('newUser')) {
+      this.router.navigate(['login']);
+    }
+  }
 
+  onboardingFinish() {
+    localStorage.setItem('newUser', 'false');
   }
 
   ngOnInit() {
